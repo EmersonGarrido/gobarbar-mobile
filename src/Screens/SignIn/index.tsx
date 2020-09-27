f3f7d6eb81db3a11acdee5206686e4fa00633021
Emerson Garrido <emersongarrido@hotmail.com.br>
2020-09-26 20:18:47 -0400

    chore: GoBarber React Native upgrade 🚀🚀

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image } from 'react-native'
+import { Image, KeyboardAvoidingView, Platform } from 'react-native'
 import * as S from './styles';
 
 import Button from '../../components/Button'
@@ -9,15 +9,31 @@ import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
   return (
+    <>
+    <KeyboardAvoidingView 
+    style={{
+      flex: 1
+    }}
+    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
     <S.Container>
-      <Image source={logoImg} />
-      <S.Title>Faça seu logon</S.Title>
+        <Image source={logoImg} />
+        <S.Title>Faça seu logon</S.Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail"/>
-      <Input name="password" icon="lock" placeholder="Senha"/>
+        <Input name="email" icon="mail" placeholder="E-mail"/>
+        <Input name="password" icon="lock" placeholder="Senha"/>
 
-      <Button onPress={() => {}}> Entrar </Button>
+        <Button onPress={() => {}}> Entrar </Button>
+        <S.ForgotPassword>
+          <S.ForgotPasswordText>Esqueci minha senha</S.ForgotPasswordText>
+        </S.ForgotPassword>
     </S.Container>
+    </KeyboardAvoidingView>
+    <S.CreateAccountButton>
+      <S.Icon name="log-in" size={20} color="#FF9000"/>
+      <S.CreateAccountButtonText>Criar uma conta</S.CreateAccountButtonText>
+    </S.CreateAccountButton>
+    </>
   );
 }
 
--- src/Screens/SignIn/styles.ts
+++ src/Screens/SignIn/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler'
+import Icons from 'react-native-vector-icons/Feather'
 
 export const Container = styled.View`
   flex: 1;
@@ -12,4 +14,38 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-family: 'Roboto-Slab-Medium';
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #F4EDE8;
+  font-size: 16px;
+  font-family: 'Roboto-Slab-Medium';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 16px 0; 
+  align-items: center;
+  justify-content: center;
+  border-top-width: 1px;
+  border-color: #232129;
+  background: #312e38;
+  flex-direction: row;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #FF9000;
+  font-size: 18px;
+  font-family: 'Roboto-Slab-Regular';
+`;
+
+export const Icon = styled(Icons)`
+  margin-right: 16px;
 `;
